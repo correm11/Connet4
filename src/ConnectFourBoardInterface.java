39fe8acc891c62b41f169bb895155e37d89d99c0
correm11 <manuel.correa@upr.edu>
2015-04-25 13:27:00 -0400

    Add some comments
    Add some comments

--- src/ConnectFourBoardInterface.java
+++ src/ConnectFourBoardInterface.java
@@ -1,18 +1,19 @@
 
 public interface ConnectFourBoardInterface {
-
+	
+	//manuel
 	public void copyBoard(ConnectFourBoardInterface source);
-	
+	//juan
 	public boolean move(int column);
-	
+	//manuel
 	public int nextPlayer();
-	
+	//juan
 	public int winner();
-	
+	//manuel
 	public int[][] getState();
-	
+	//juan
 	public int[] getColumns();
-	
+	//manuel
 	public int getColumn(int column);
 	
 }
