28998f0654188b05e1cdff478386cdc54a89527f
correm11 <manuel.correa@upr.edu>
2015-04-25 14:15:55 -0400

    Implement many methods of board class.
    Implement many methods of board class and add some varibles.

--- src/Board.java
+++ src/Board.java
@@ -4,21 +4,33 @@ public class Board implements ConnectFourBoardInterface{
 	private int[][] board;
 	private int[] column;
 	private int player;
+	private int amountOfRows;
+	private int amountOfColums;
 	
 	/**
 	 * Constructor
+	 * @param amountOfColumns 
+	 * @param amountOfRows
+	 * @param player 
 	 */
-	public Board(int player) {
+	public Board(int player, int amountOfColumns, int amountOfRows) {
 		
-		this.board = new int[6][7];
-		this.column = new int[7];
-		this.player = player;
+		this.amountOfColums = amountOfColumns;
+		this.amountOfRows = amountOfRows;
+		this.board = new int[amountOfRows][amountOfColumns];
+		this.column = new int[amountOfColumns];
+		this.player = player;		
 		
 	}
-
+		
 	@Override
 	public void copyBoard(ConnectFourBoardInterface source) {
-		// TODO Auto-generated method stub
+		
+		this.player = source.nextPlayer();
+		
+		this.column = source.getColumns();
+		
+		this.board = source.getState();
 		
 	}
 
@@ -30,8 +42,7 @@ public class Board implements ConnectFourBoardInterface{
 
 	@Override
 	public int nextPlayer() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.player;
 	}
 
 	@Override
@@ -42,20 +53,36 @@ public class Board implements ConnectFourBoardInterface{
 
 	@Override
 	public int[][] getState() {
-		// TODO Auto-generated method stub
-		return null;
+		int[][] result = new int[this.amountOfRows][this.amountOfColums];
+		
+		for(int i = 0; i < this.amountOfRows; i++){
+			for(int j = 0; j < this.amountOfColums; j++){
+				result[i][j] = this.board[i][j];
+			}
+		}
+				
+		return result;
 	}
 
 	@Override
 	public int[] getColumns() {
-		// TODO Auto-generated method stub
-		return null;
+		int[] result = new int[this.column.length];
+		
+		for(int i = 0; i < this.column.length; i++){
+			result[i] = this.getColumn(i);
+		}
+		
+		return result;
 	}
 
 	@Override
 	public int getColumn(int column) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		if(column < 0 || column >6){
+			throw new IndexOutOfBoundsException();
+		}
+		
+		return this.column[column];
 	}	
 
 }
