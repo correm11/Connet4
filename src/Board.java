c5313ce06795d4488f722b9578269406be6eac17
correm11 <manuel.correa@upr.edu>
2015-04-25 13:46:47 -0400

    Add board private global variables for board class.
    Add board private global variables for board class, create the
    constructor and retrieved interface methods.

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,61 @@
 
-public class Board {
+public class Board implements ConnectFourBoardInterface{
 	
+	private int[][] board;
+	private int[] column;
+	private int player;
 	
+	/**
+	 * Constructor
+	 */
+	public Board(int player) {
+		
+		this.board = new int[6][7];
+		this.column = new int[7];
+		this.player = player;
+		
+	}
+
+	@Override
+	public void copyBoard(ConnectFourBoardInterface source) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean move(int column) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int nextPlayer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int winner() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[][] getState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getColumns() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getColumn(int column) {
+		// TODO Auto-generated method stub
+		return 0;
+	}	
 
 }
