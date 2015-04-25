4bb940fe9387f8af33083e35deefc55d0bb7a64f
kebinbin <kevin.castillo@upr.edu>
2015-04-25 17:47:54 -0400

    MinMaxPlayer

--- src/MinMaxPlayer.java
+++ src/MinMaxPlayer.java
@@ -1,5 +1,63 @@
 
-public class MinMaxPlayer {
+public class MinMaxPlayer implements MinMaxPlayerInterface {
+
+	@Override
+	public int getMin(Node n) {
+		if(n.isLeaf() || n.getBoard().winner()!=-1){//if a leaf or game ended (terminal node)
+			return this.getUtilityValue(n);
+		}
+		else{
+			n.setValue(999);
+			for(Node child: n.getChildren()){
+				int maxValue = getMin(child);
+				if(maxValue < n.getValue()){
+					n.setValue(maxValue);
+				}
+			}
+			return n.getValue();
+		}
+	}
+
+	@Override
+	public int getMax(Node n) {
+		if(n.isLeaf() || n.getBoard().winner()!=-1){//if a leaf or game ended (terminal node)
+			return this.getUtilityValue(n);
+		}
+		else{
+			n.setValue(-999);
+			for(Node child: n.getChildren()){
+				int minValue = getMin(child);
+				if(minValue > n.getValue()){
+					n.setValue(minValue);
+				}
+			}
+			return n.getValue();
+		}
+	}
+
+	@Override
+	public int minMaxResult(Node n) {
+		
+		return 0;
+	}
+
+	@Override
+	public void minMaxTurn(Node n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getDepth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getUtilityValue(Node n) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 	
 
--- src/MinMaxPlayerInterface.java
+++ src/MinMaxPlayerInterface.java
@@ -1,17 +1,15 @@
 
 public interface MinMaxPlayerInterface {
 
-	public int getMin(Graph g);
+	public int getMin(Node n);
 	
-	public int getMax(Graph g);
+	public int getMax(Node n);
 	
-	public int minMaxResult(Graph g);
+	public int minMaxResult(Node n);
 	
-	public boolean isLeaf(Graph g);
-	
-	public void minMaxTurn(Graph g);
+	public void minMaxTurn(Node n);
 	
 	public int getDepth();
 	
-	public int getHeuristic();
+	public int getUtilityValue(Node n);
 }
